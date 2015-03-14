cdbe5686890e3faea641316ad791129c6edc9bed
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-14 16:06:34 +0300

    [^] refactored 'new' task

--- lib/wimdu/cli.rb
+++ lib/wimdu/cli.rb
@@ -23,71 +23,12 @@ module Wimdu
 
       say "Starting with new property #{slug}."
 
-      loop do
-        property.awaits = 'title'
-        property.title = ask('Title: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
-
-      loop do
-        property.awaits = 'address'
-        property.address = ask('Address: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
-
-      loop do
-        property.awaits = 'rate'
-        property.rate = ask('Nightly rate in EUR: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
-
-      loop do
-        property.awaits = 'max_guests'
-        property.max_guests = ask('Max guests: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
-
-      loop do
-        property.awaits = 'email'
-        property.email = ask('Email: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
-
-      loop do
-        property.awaits = 'phone'
-        property.phone = ask('Phone: ')
-        if property.valid?
-          property.save
-          break
-        else
-          puts property.errors.full_messages
-        end
-      end
+      ask_for(property, :title)
+      ask_for(property, :address)
+      ask_for(property, :rate)
+      ask_for(property, :max_guests)
+      ask_for(property, :email)
+      ask_for(property, :phone)
 
       say "Great job! Listing #{property.slug} is complete!"
     end
--- lib/wimdu/helpers.rb
+++ lib/wimdu/helpers.rb
@@ -5,4 +5,35 @@ module Helpers
       break random_slug unless Property.exists?(slug: random_slug)
     end
   end
+
+  def ask_for(property, attr)
+    case attr
+    when :title
+      property.awaits = 'title'
+      property.title = ask('Title: ')
+    when :address
+      property.awaits = 'address'
+      property.address = ask('Address: ')
+    when :rate
+      property.awaits = 'rate'
+      property.rate = ask('Nightly rate in EUR: ')
+    when :max_guests
+      property.awaits = 'max_guests'
+      property.max_guests = ask('Max guests: ')
+    when :email
+      property.awaits = 'email'
+      property.email = ask('Email: ')
+    when :phone
+      property.awaits = 'phone'
+      property.phone = ask('Phone: ')
+    else
+    end
+
+    if property.valid?
+      property.save
+    else
+      puts property.errors.full_messages
+      ask_for(property, attr)
+    end
+  end
 end
